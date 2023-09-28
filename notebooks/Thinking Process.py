89573ece96cf9f4b5ca55b8519f35a4730c236f0
Ido Michealovich <40335982+Th3Tr00p3r@users.noreply.github.com>
2023-09-28 12:28:42 +0300

    Completed sanity check for Doc2Vec Model, and trained one over 100K privacy policies. Updated gitignore

--- notebooks/Thinking Process.py
+++ notebooks/Thinking Process.py
@@ -72,6 +72,7 @@ REPO_PATH = Path("D:/MEGA/Programming/ML/Data/") / "privacy-policy-historical"
 
 # %%
 import random
+import re
 from dataclasses import InitVar, dataclass
 from pathlib import Path
 from typing import Generator, Iterable
@@ -79,12 +80,29 @@ from typing import Generator, Iterable
 import gensim
 import nltk
 from nltk.corpus import stopwords
+from smart_open import open
 
 # Download the stopwords dataset (if not already downloaded)
 nltk.download("stopwords")
 
 # Get the list of english stopwords
 STOP_WORDS = set(stopwords.words("english"))
+
+URL_PATTERN = r"(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+
+
+def reservoir_sampling(iterator, k):
+
+    sample = []
+    for i, item in enumerate(iterator):
+        if i < k:
+            sample.append(item)
+        else:
+            j = random.randint(0, i)
+            if j < k:
+                sample[j] = item
+
+    return sample
 
 
 @dataclass
@@ -94,13 +112,19 @@ class TagDocumentPairIterator:
     policy_paths: InitVar[Iterable[Path]]
     n: int = None
     seed: int = None
+    should_filter_stopwords: bool = False
+    url_pattern: str = URL_PATTERN
+
+    @property
+    def doc_gen(self):
+        return (doc for tag, doc in self)
 
     def __post_init__(self, policy_paths):
         random.seed(self.seed)
-        self.policy_paths = list(policy_paths)
         if self.n is None:
-            self.n = len(self.policy_paths)
-        self.policy_paths = random.sample(self.policy_paths, self.n)
+            self.policy_paths = list(policy_paths)
+        else:
+            self.policy_paths = reservoir_sampling(policy_paths, self.n)
 
     def __iter__(self):
         for fpath in self.policy_paths:
@@ -114,15 +138,22 @@ class TagDocumentPairIterator:
             _, *doc_lines = f.read().split("\n")
             doc = "\n".join(doc_lines)
 
+        # Replace URLs with "<URL>"
+        if self.url_pattern:
+            doc = re.sub(self.url_pattern, "<URL>", doc)
+
         # tokenize the text
         simp_proc_doc = gensim.utils.simple_preprocess(doc)
 
         # remove stopwords and return
-        return [word for word in simp_proc_doc if word not in STOP_WORDS]
+        if self.should_filter_stopwords:
+            return [word for word in simp_proc_doc if word not in STOP_WORDS]
+        else:
+            return simp_proc_doc
 
 
 # get all privacy policy markdown file paths in a generator
-policy_path_gen = [fpath for fpath in REPO_PATH.rglob("*.md") if fpath.name != "README.md"]
+policy_path_gen = lambda: (fpath for fpath in REPO_PATH.rglob("*.md") if fpath.name != "README.md")
 
 print("Done.")
 
@@ -130,16 +161,6 @@ print("Done.")
 # ### 1.1.3 Saving the transformed data (Loading)
 #
 # Since we already have the raw data locally, and since we do not expect more data to come in, we can avoid saving the data and just lazily transform it as needed. This means the loaded data becomes just the file paths.
-#
-# Let's create a function for sampling from our database:
-
-# %%
-N = 10
-nl = "\n"
-SEED = 17
-print(
-    f"sampling {N} companies/URLs:{nl}{nl.join([item[0] for item in TagDocumentPairIterator(policy_path_gen, N, SEED)])}"
-)
 
 # %% [markdown]
 # # 2. Preliminary EDA
@@ -148,11 +169,12 @@ print(
 # We can try using only a subset of the database for now (say 10K documents)
 
 # %%
+SEED = 17
 N = 10_000
 # N = 10
-company_urls, corpus = list(zip(*TagDocumentPairIterator(policy_path_gen, N, SEED)))
+corpus = TagDocumentPairIterator(policy_path_gen(), N, SEED)
 
-print(f"Loaded and transformed {N} privacy policies.")
+print(f"Loaded corpus of {N:,} privacy policies.")
 
 # %% [markdown]
 # Let's take a look at the distribution of PP lengths (number of tokens):
@@ -161,9 +183,9 @@ print(f"Loaded and transformed {N} privacy policies.")
 import matplotlib.pyplot as plt
 import numpy as np
 
-pp_lengths = np.array([len(tokens) for tokens in corpus])
+pp_lengths = np.array([len(tokens) for url, tokens in corpus])
 
-_ = plt.hist(pp_lengths, 100)
+_ = plt.hist(pp_lengths, 80)
 
 print(f"PP length range: {pp_lengths.min()} - {pp_lengths.max():,} tokens")
 print(f"median PP length: {np.median(pp_lengths):,} tokens")
@@ -176,7 +198,7 @@ print(f"median PP length: {np.median(pp_lengths):,} tokens")
 # %%
 from gensim.corpora import Dictionary
 
-dct = Dictionary(corpus)
+dct = Dictionary(corpus.doc_gen)
 
 print(
     f"Dictionary was created by processing {dct.num_pos:,} tokens from a corpus of {dct.num_docs:,} documents."
@@ -186,14 +208,11 @@ print(f"Each document, on average, contains {dct.num_nnz // dct.num_docs:,} uniq
 
 # %% [markdown]
 # First, let's visualize out the most frequent words in the entire corpus as a word-cloud:
-#
-# from typing import Dict
-#
-# import matplotlib.pyplot as plt
-
-from typing import Dict
 
 # %%
+from typing import Dict
+
+import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
 
@@ -240,7 +259,7 @@ from copy import deepcopy
 filtered_dct = deepcopy(dct)
 filtered_dct.filter_extremes()
 
-print(f"After filtering extremens, Dictionary contains {len(filtered_dct)} unique tokens")
+print(f"After filtering extremens, Dictionary contains {len(filtered_dct):,} unique tokens")
 
 word_totalfreq_dict = {token: filtered_dct.cfs[id] for token, id in filtered_dct.token2id.items()}
 word_docfreq_dict = {token: filtered_dct.dfs[id] for token, id in filtered_dct.token2id.items()}
@@ -253,6 +272,11 @@ display_wordcloud(word_docfreq_dict)
 
 # %% [markdown]
 # What immediately stands out is the difference between the "total frequency" and "per-document frequency" before and after filtering the most common words. In the "total frequency" picture, we are just seeing less common words (the most common ones being ignored). In the "per-document frequency" picture, this enables us to see past the noise.
+#
+# Look for the frequency of specific words (such as "url"):
+
+# %%
+# TODO
 
 # %% [markdown]
 # # 3. Modeling
@@ -261,11 +285,15 @@ display_wordcloud(word_docfreq_dict)
 # 1) Enabling cluster analysis and visualization
 # 2) Similarity comparison between PPs
 # 3) Inferring vector embedding for non-corpus policies
-#
-# TODO: It remains to be seen if this is feasible for the complete dataset on my maching
 
 # %%
+import multiprocessing as mp
+import time
+from winsound import Beep
+
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+
+N_CPU_CORES = mp.cpu_count() // 2  # /2 due to hyperthreading,
 
 
 @dataclass
@@ -283,40 +311,83 @@ class TaggedDocumentIterator:
 
 
 # sample 10,000 PPs as a first attempt
-N = 10_000
-url_doc_corpus = TagDocumentPairIterator(policy_path_gen, N, SEED)
+N = 100_000
+url_doc_corpus = TagDocumentPairIterator(policy_path_gen(), N, SEED, should_filter_stopwords=False)
 
 # Create tagged documents
 train_data = TaggedDocumentIterator(url_doc_corpus)
 
 # Initialize and train the Doc2Vec model
-model = Doc2Vec()
+model_kwargs = {
+    "vector_size": 400,
+    #     "workers": N_CPU_CORES,
+}
+model = Doc2Vec(**model_kwargs)
 
 # Build vocabulary
 model.build_vocab(train_data)
 
 # Train the model
+tic = time.perf_counter()
 model.train(train_data, total_examples=model.corpus_count, epochs=model.epochs)
+print(f"Training timing: {(time.perf_counter() - tic)/60:.1f} mins")
 
 # Save the trained model for future use
 PROJECT_ROOT = Path.cwd().parent
-model.save(PROJECT_ROOT / "models" / "privacy_policy_doc2vec.model")
+model.save(str(PROJECT_ROOT / "models" / "privacy_policy_doc2vec.model"))
+
+# Done!
+print("Done training.")
+Beep(1000, 500)  # Beep at 1000 Hz for 500 ms
 
 # %% [markdown]
-# Sanity check - check that most documents are most similar to themselves
+# Loading the latest save model instead:
+
+# %%
+from gensim.models import Doc2Vec
+
+PROJECT_ROOT = Path.cwd().parent
+
+# Replace 'your_model_file_path' with the actual path to your saved model file
+model = Doc2Vec.load(str(PROJECT_ROOT / "models" / "privacy_policy_doc2vec.model"))
+
+# Done
+print("Model Loaded")
+# TODO: print some model details
+
+# %% [markdown]
+# Sanity check - check that documents are most similar to themselves
 
 # %%
 import collections
 
+# Set the number of top similar documents to consider
+SAMPLE_SIZE = 1000
+TOP_N = 2
+
 ranks = []
 second_ranks = []
-for tagged_doc in train_data:
-    inferred_vector = model.infer_vector(tagged_doc.words)
-    sims = model.dv.most_similar([inferred_vector], topn=len(model.dv))
-    rank = [docid for docid, sim in sims].index(tagged_doc.tags[0])
+for idx, tag in enumerate(reservoir_sampling(model.dv.index_to_key, SAMPLE_SIZE)):
+    # keep track
+    if not (idx + 1) % (SAMPLE_SIZE // 10):
+        print(f"{(idx+1)/(SAMPLE_SIZE):.1%}... ", end="")
+
+    # Calculate similarities only for the TOP_N similar documents for the current inferred vector
+    sims = model.dv.most_similar([model.dv[tag]], topn=TOP_N)
+
+    # Find the rank of the tag in the top N
+    try:
+        rank = [docid for docid, sim in sims].index(tag)
+    except ValueError:
+        # Handle the case where the tag is not found in sims
+        rank = -1  # Or any other value that indicates "not found"
     ranks.append(rank)
 
     second_ranks.append(sims[1])
+print(" Done.")
 
 counter = collections.Counter(ranks)
-print(counter)
+print("counter: ", counter)
+
+# %%
+second_ranks
